2ebbe4dbd68b7804346abc1acf97fd7342e71d5a
caiocvlopes <caiocvlopes@gmail.com>
2024-05-16 17:32:05 -0300

    modificando index

--- djangoapp/app/views.py
+++ djangoapp/app/views.py
@@ -1,13 +1,17 @@
-from django.http import HttpRequest, HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from . import tasks
 
 
 # Create your views here.
 def index(request: HttpRequest) -> HttpResponse:
-    ctx = {'page_section': 'Home'}
-    return render(request, 'index.html', ctx)
 
+    ctx = {
+        'login_url': reverse('login'),
+        'register_url': reverse('register')
+    }
+    return render(request, 'index.html', ctx)
 
 
 def celery_hello_world(request: HttpResponse) -> JsonResponse:
