c32417c10c590bb61f032938697e702d26239479
caiocvlopes <caiocvlopes@gmail.com>
2024-05-20 16:45:49 -0300

    criando links entre telas através da navbar

--- djangoapp/app/urls.py
+++ djangoapp/app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('', views.home, name='home'),
+    path('service_orders/', views.service_orders, name='service_orders'),
+    path('config/', views.config, name='config'),
     path('celery_hello_world/', views.celery_hello_world, name='celery_hello_world')
 ]
--- djangoapp/app/views.py
+++ djangoapp/app/views.py
@@ -5,13 +5,50 @@ from . import tasks
 
 
 # Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
+def welcome(request: HttpRequest) -> HttpResponse:
 
     ctx = {
         'login_url': reverse('login'),
         'register_url': reverse('register')
     }
-    return render(request, 'index.html', ctx)
+    return render(request, 'app/welcome.html', ctx)
+
+
+def home(request: HttpRequest) -> HttpResponse:
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('welcome'))
+
+    ctx = {
+        'home': True,
+        'is_staff': request.user.is_staff,
+        'logout_url': reverse('logout')
+    }
+    return render(request, 'app/index.html', ctx)
+
+
+def service_orders(request: HttpRequest) -> HttpResponse:
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('welcome'))
+
+    ctx = {
+        'service_orders': True,
+        'is_staff': request.user.is_staff
+    }
+    return render(request, 'app/service_orders.html', ctx)
+
+
+def config(request: HttpRequest) -> HttpResponse:
+
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('welcome'))
+
+    ctx = {
+        'config': True,
+        'is_staff': request.user.is_staff
+    }
+    return render(request, 'app/config.html', ctx)
 
 
 def celery_hello_world(request: HttpResponse) -> JsonResponse:
